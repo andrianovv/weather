f3ab3abc12f87a6bee034292295dbc30fdaa5b85
andrianovv <andrianov.vitali@gmail.com>
2015-12-03 10:44:29 +0200

    Update geolocation.js

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -13,7 +13,7 @@ function geolocation(lang, fn_Ok, fn_Error)
 				{
             $.getJSON(
                 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + position.coords.latitude + '&lon=' +
-                position.coords.longitude + '&cnt=16&units=metric' + '&lang=' + lang + '&callback=?',
+                position.coords.longitude + '&cnt=16&units=metric&APPID=85dd9ac97b8b9d0cbc5f814a5b96ffa6' + '&lang=' + lang + '&callback=?',
                 function (response) 
 								{
                     localStorage.weatherCache = JSON.stringify(     // Store the cache
