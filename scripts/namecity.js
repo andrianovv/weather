d56381c25a263306b71fa6e9a1ff0a65bae4bbd9
andrianovv <andrianov.vitali@gmail.com>
2015-12-03 10:45:03 +0200

    chnge API key

--- scripts/namecity.js
+++ scripts/namecity.js
@@ -2,7 +2,7 @@ function name_city(lang, fn_Ok, fn_Error, city_name)
 {
 	$.getJSON(
     'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
-     + city_name + '&cnt=16&units=metric' + '&lang=' + lang + '&callback=?',
+     + city_name + '&cnt=16&units=metric&APPID=85dd9ac97b8b9d0cbc5f814a5b96ffa6' + '&lang=' + lang + '&callback=?',
     function (data) 
 		{
       fn_Ok.call(this, data);
